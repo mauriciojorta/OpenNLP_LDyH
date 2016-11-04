dc25da6c7a1e76554801e992a09c909ac9df84f8
Mauricio José Orta Rodríguez <sextus70@gmail.com>
2016-11-04 11:23:08 +0000

    Comentarios de clases de Lectura y Salida

--- src/main/java/Aplicacion/Lector.java
+++ src/main/java/Aplicacion/Lector.java
@@ -4,12 +4,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * Clase para la lectura de ficheros
+ * @author Mauricio
+ *
+ */
 public class Lector {
-	private File file;
-	private Scanner reader;
+	private File file; /// Fichero a leer
+	private Scanner reader; ///Lector de fichero
 
 	/**
-	 * @param file
+	 * Constructor de lector
+	 * Recibe el nombre y la dirección de un fichero y crea un lector para leerlo
+	 * @param nfile - Nombre y dirección de fichero
 	 * @throws FileNotFoundException
 	 */
 	public Lector(String nfile) throws FileNotFoundException {
--- src/main/java/Aplicacion/Salida.java
+++ src/main/java/Aplicacion/Salida.java
@@ -4,16 +4,32 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
+/**
+ * Clase para la  generación de ficheros de salida tokenizados
+ * @author Mauricio
+ *
+ */
 public class Salida {
-	private PrintStream escritor;
-	private String[] tokens;
+	private PrintStream escritor; ///Flujo de salida
+	private String[] tokens; ///Tokens a escribir
 
+	/**
+	 * Constructor de Salida
+	 * Crea un flujo de salida en un nuevo fichero o sobre uno ya existente y recibe un conjunto de
+	 * tokens a escribir en él.
+	 * @param nfile - Nombre y dirección de fichero
+	 * @param tokens - Tokens
+	 * @throws FileNotFoundException
+	 */
 	public Salida(String nfile, String[] tokens) throws FileNotFoundException
 	{
 		this.escritor = new PrintStream(new FileOutputStream(nfile));
 		this.tokens = tokens;
 	}
 
+	/**
+	 * Escribe los tokens en el fichero de salida
+	 */
 	public void escribirSalida()
 	{
 		for( String token : tokens )
